17b5bfe99c12ec1616eebadd2f8e6a318ce1a808
Yuta <yuta256dev@gmail.com>
2025-10-29 15:47:52 +0900

    perf(pkg-openssl): Improve performance
    - Switch compiler from GCC to Clang - Enable AVX instructions

--- pkgs/openssl/make.sh
+++ pkgs/openssl/make.sh
@@ -16,13 +16,15 @@ dependencies=(
 )
 
 build() {
-  local CFLAGS="-O2 -pipe -march=$MARCH_LEVEL -mtune=$MTUNE_LEVEL"
+  local CC="clang"
+  local CFLAGS="-O2 -pipe $AVX_LEVEL -march=$MARCH_LEVEL -mtune=$MTUNE_LEVEL"
   local configure_options=(
     --prefix=/usr
     --openssldir=/etc/ssl
     --libdir=lib
     shared
     zlib-dynamic
+    CC="$CC"
     CFLAGS="$CFLAGS"
   )
 
