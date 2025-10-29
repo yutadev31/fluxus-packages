22088eba8a6a4eb05885ca2422be8cc0614e1197
Yuta <yuta256dev@gmail.com>
2025-10-29 13:43:46 +0900

    perf(pkg-binutils): Improve performance
    - Switch compiler from GCC to Clang - Enable AVX instructions

--- pkgs/binutils/make.sh
+++ pkgs/binutils/make.sh
@@ -16,7 +16,8 @@ dependencies=(
 )
 
 build() {
-  local CFLAGS="-O2 -pipe -march=$MARCH_LEVEL -mtune=$MTUNE_LEVEL"
+  local CC="clang"
+  local CFLAGS="-O2 -pipe $AVX_LEVEL -march=$MARCH_LEVEL -mtune=$MTUNE_LEVEL"
   local configure_options=(
     --prefix=/usr
     --sysconfdir=/etc
@@ -28,6 +29,7 @@ build() {
     --enable-new-dtags
     --with-system-zlib
     --enable-default-hash-style=gnu
+    CC="$CC"
     CFLAGS="$CFLAGS"
   )
 
