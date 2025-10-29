d66fd892031251bd94239f488fe8e40609fceafc
Yuta <yuta256dev@gmail.com>
2025-10-29 15:00:00 +0900

    perf(pkg-gawk): Improve performance
    - Switch compiler from GCC to Clang - Enable AVX instructions

--- pkgs/gawk/make.sh
+++ pkgs/gawk/make.sh
@@ -21,10 +21,12 @@ prepare() {
 }
 
 build() {
-  local CFLAGS="-O2 -pipe -march=$MARCH_LEVEL -mtune=$MTUNE_LEVEL"
+  local CC="clang"
+  local CFLAGS="-O2 -pipe $AVX_LEVEL -march=$MARCH_LEVEL -mtune=$MTUNE_LEVEL"
   local configure_options=(
     --prefix=/usr
     --libexecdir=/usr/lib
+    CC="$CC"
     CFLAGS="$CFLAGS"
   )
 
